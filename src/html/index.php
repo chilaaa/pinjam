9c68db613416910510a73ed44b4d36be7792dbe2
Adam02RPLC <adambahynmaulana62@smp.belajar.id>
2023-11-14 09:30:20 +0700

    push lagi

--- src/html/index.php
+++ src/html/index.php
@@ -45,12 +45,12 @@ $nama_user = $_SESSION['nama_user'];
                 <span class="hide-menu">Dashboard</span>
               </a>
             </li>
-            <select class="form-select">
-                      <option value="1">March 2023</option>
-                      <option value="2">April 2023</option>
-                      <option value="3">May 2023</option>
-                      <option value="4">June 2023</option>
-                    </select>
+            <!-- <select class="form-select">
+              <option value="1">March 2023</option>
+              <option value="2">April 2023</option>
+              <option value="3">May 2023</option>
+              <option value="4">June 2023</option>
+            </select> -->
             <li class="nav-small-cap">
               <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
               <span class="hide-menu">Forms</span>
@@ -119,87 +119,87 @@ $nama_user = $_SESSION['nama_user'];
                   <div class="mb-3">
                     <input class="form-control" type="text" id="pinjamSearch" placeholder="Search Tabel Pinjam">
                   </div>
-                    <table class="table text-nowrap mb-0 align-middle">
-                      <thead class="text-dark fs-4">
+                  <table class="table text-nowrap mb-0 align-middle">
+                    <thead class="text-dark fs-4">
+                      <tr>
+                        <th class="border-bottom-0">
+                          <h6 class="fw-semibold mb-0">Id</h6>
+                        </th>
+                        <th class="border-bottom-0">
+                          <h6 class="fw-semibold mb-0">Nama Peminjam</h6>
+                        </th>
+                        <th class="border-bottom-0">
+                          <h6 class="fw-semibold mb-0">Tanggal</h6>
+                        </th>
+                        <th class="border-bottom-0">
+                          <h6 class="fw-semibold mb-0">Ruangan</h6>
+                        </th>
+                        <th class="border-bottom-0">
+                          <h6 class="fw-semibold mb-0">Unit</h6>
+                        </th>
+                        <th class="border-bottom-0">
+                          <h6 class="fw-semibold mb-0">Jam Peminjaman</h6>
+                        </th>
+                        <th class="border-bottom-0">
+                          <h6 class="fw-semibold mb-0">Keterangan</h6>
+                        </th>
+                        <th class="border-bottom-0">
+                          <h6 class="fw-semibold mb-0"></h6>
+                        </th>
+                        <th class="border-bottom-0">
+                          <h6 class="fw-semibold mb-0"></h6>
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <?php $query = mysqli_query($conn, "SELECT * FROM jadwal_pinjam"); ?>
+                      <?php while ($row = mysqli_fetch_assoc($query)) : ?>
                         <tr>
-                          <th class="border-bottom-0">
-                            <h6 class="fw-semibold mb-0">Id</h6>
-                          </th>
-                          <th class="border-bottom-0">
-                            <h6 class="fw-semibold mb-0">Nama Peminjam</h6>
-                          </th>
-                          <th class="border-bottom-0">
-                            <h6 class="fw-semibold mb-0">Tanggal</h6>
-                          </th>
-                          <th class="border-bottom-0">
-                            <h6 class="fw-semibold mb-0">Ruangan</h6>
-                          </th>
-                          <th class="border-bottom-0">
-                            <h6 class="fw-semibold mb-0">Unit</h6>
-                          </th>
-                          <th class="border-bottom-0">
-                            <h6 class="fw-semibold mb-0">Jam Peminjaman</h6>
-                          </th>
-                          <th class="border-bottom-0">
-                            <h6 class="fw-semibold mb-0">Keterangan</h6>
-                          </th>
-                          <th class="border-bottom-0">
-                            <h6 class="fw-semibold mb-0"></h6>
-                          </th>
-                          <th class="border-bottom-0">
-                            <h6 class="fw-semibold mb-0"></h6>
-                          </th>
+                          <td class="border-bottom-0">
+                            <h6 class="fw-semibold mb-0"><?php echo $row['id_pinjam'] ?></h6>
+                          </td>
+                          <td class="border-bottom-0">
+                            <h6 class="fw-semibold mb-1"><?php echo $row['nama_peminjam'] ?></h6>
+                          </td>
+                          <td class="border-bottom-0">
+                            <h6 class="fw-semibold mb-1"><?php echo $row['tanggal'] ?></h6>
+                          </td>
+                          <td class="border-bottom-0">
+                            <?php
+                            $ruangan_id = $row['id_ruang'];
+                            $ruangan_query = mysqli_query($conn, "SELECT nama_ruang FROM m_ruang WHERE id_ruang = '$ruangan_id'");
+                            $ruangan_data = mysqli_fetch_assoc($ruangan_query);
+                            $ruangan_nama = $ruangan_data['nama_ruang'];
+                            echo '<h6 class="fw-semibold mb-1">' . $ruangan_nama . '</h6>';
+                            echo '<span class="fw-normal">' . $row['id_ruang'] . '</span>';
+                            ?>
+                          </td>
+
+                          <td class="border-bottom-0">
+                            <?php
+                            $unit_id = $row['id_unit'];
+                            $unit_query = mysqli_query($conn, "SELECT nama_unit FROM m_unit WHERE id_unit = '$unit_id'");
+                            $unit_data = mysqli_fetch_assoc($unit_query);
+                            $unit_nama = $unit_data['nama_unit'];
+                            echo '<h6 class="fw-semibold mb-1">' . $unit_nama . '</h6>';
+                            echo '<span class="fw-normal">' . $row['id_unit'] . '</span>';
+                            ?>
+                          </td>
+
+                          <td class="border-bottom-0">
+                            <h6 class="fw-semibold mb-1"><?php echo $row['jam_awal'] ?> - <?php echo $row['jam_akhir'] ?></h6>
+                          </td>
+                          <td class="border-bottom-0">
+                            <h6 class="fw-semibold mb-1"><?php echo $row['keterangan'] ?></h6>
+                          </td>
+                          <td class="border-bottom-0">
+                            <a href="form-update.php?id_pinjam=<?php echo $row['id_pinjam'] ?>" class="btn btn-primary m-1">Update</a>
+                          </td>
                         </tr>
-                      </thead>
-                      <tbody>
-                        <?php $query = mysqli_query($conn, "SELECT * FROM jadwal_pinjam"); ?>
-                        <?php while ($row = mysqli_fetch_assoc($query)) : ?>
-                          <tr>
-                            <td class="border-bottom-0">
-                              <h6 class="fw-semibold mb-0"><?php echo $row['id_pinjam'] ?></h6>
-                            </td>
-                            <td class="border-bottom-0">
-                              <h6 class="fw-semibold mb-1"><?php echo $row['nama_peminjam'] ?></h6>
-                            </td>
-                            <td class="border-bottom-0">
-                              <h6 class="fw-semibold mb-1"><?php echo $row['tanggal'] ?></h6>
-                            </td>
-                            <td class="border-bottom-0">
-                              <?php
-                              $ruangan_id = $row['id_ruang'];
-                              $ruangan_query = mysqli_query($conn, "SELECT nama_ruang FROM m_ruang WHERE id_ruang = '$ruangan_id'");
-                              $ruangan_data = mysqli_fetch_assoc($ruangan_query);
-                              $ruangan_nama = $ruangan_data['nama_ruang'];
-                              echo '<h6 class="fw-semibold mb-1">' . $ruangan_nama . '</h6>';
-                              echo '<span class="fw-normal">' . $row['id_ruang'] . '</span>';
-                              ?>
-                            </td>
+                      <?php endwhile; ?>
 
-                            <td class="border-bottom-0">
-                              <?php
-                              $unit_id = $row['id_unit'];
-                              $unit_query = mysqli_query($conn, "SELECT nama_unit FROM m_unit WHERE id_unit = '$unit_id'");
-                              $unit_data = mysqli_fetch_assoc($unit_query);
-                              $unit_nama = $unit_data['nama_unit'];
-                              echo '<h6 class="fw-semibold mb-1">' . $unit_nama . '</h6>';
-                              echo '<span class="fw-normal">' . $row['id_unit'] . '</span>';
-                              ?>
-                            </td>
-
-                            <td class="border-bottom-0">
-                              <h6 class="fw-semibold mb-1"><?php echo $row['jam_awal'] ?> - <?php echo $row['jam_akhir'] ?></h6>
-                            </td>
-                            <td class="border-bottom-0">
-                              <h6 class="fw-semibold mb-1"><?php echo $row['keterangan'] ?></h6>
-                            </td>
-                            <td class="border-bottom-0">
-                              <a href="form-update.php?id_pinjam=<?php echo $row['id_pinjam'] ?>" class="btn btn-primary m-1">Update</a>
-                            </td>
-                          </tr>
-                        <?php endwhile; ?>
-
-                      </tbody>
-                    </table>
+                    </tbody>
+                  </table>
                 </div>
               </div>
             </div>
